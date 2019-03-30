e61fe76a69fbcae75ff1d5b8c1870001f1ae35b8
Abinav Rameesh <abramees@microsoft.com>
2019-03-30 11:47:08 -0700

    Adding success and failure details classes

---
+++ Microsoft.CosmosDB.PITRWithRestore/Backup/BackupSuccessDocument.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace Microsoft.CosmosDB.PITRWithRestore.Backup
+{
+    internal sealed class BackupSuccessDocument
+    {
+        /// <summary>
+        /// Name of the Blob Storage container, with the backups to be restored
+        /// </summary>
+        [JsonProperty(PropertyName = "containerName")]
+        public string ContainerName { get; set; }
+
+        /// <summary>
+        /// id of the document once it is persisted in Cosmos DB
+        /// </summary>
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty(PropertyName = "documentCount")]
+        public int DocumentCount { get; set; }
+    }
+}
